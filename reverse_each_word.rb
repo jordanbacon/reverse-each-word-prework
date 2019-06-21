c7d4fa8269660ef6fa3685ac85f7e4668819b3b4
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 00:47:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-    array = string.split
-    array.collect do |x|
-        x.reverse!
-    end
-    array.join (" ")
+  array = string.split
+  array.collect do |x|
+    x.reverse!
+  end
+  array.join (" ")
 end
