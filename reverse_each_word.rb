0090695fb157383d40d14f1db295a34af1a55a8b
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 00:44:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_each_word(sentance)
-  reverse_each_word = sentance.split(' ')
-  reverse_words = reverse_each_word.collect { |word| word.reverse}
-  reverse_words.join(' ')
+def reverse_each_word(string)
+    array = string.split
+    array.collect do |x|
+        x.reverse!
+    end
+    array.join (" ")
 end
